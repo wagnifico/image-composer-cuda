909085cbecd3572a9153e33b9bc0aaf87bebbb90
Wagner Gonçalves Pinto <73096490+wagnifico@users.noreply.github.com>
2025-11-05 14:25:29 +0000

    make functions.h compliant, remove redundant comments

--- src/functions.h
+++ src/functions.h
@@ -13,35 +13,33 @@
 #include <ImagesCPU.h>
 #include <ImagesNPP.h>
 
-
-
 /// @brief Converts a FreeImage bitmap to an NVIDIA NPP-compatible image.
 /// @tparam T The type of the NPP image (e.g., npp::ImageCPU_8u_C4).
-/// @param pBitmap Pointer to the loaded FreeImage bitmap (FIBITMAP).
-/// @param outImage Reference to the output NPP image object.
+/// @param bitmap Pointer to the loaded FreeImage bitmap (FIBITMAP).
+/// @param out_image Reference to the output NPP image object.
 template <typename T>
 void convertFreeImageToNppImage(
-    FIBITMAP *pBitmap, T &outImage)
+    FIBITMAP *bitmap, T &out_image)
 {
     // Get image dimensions and pitch from the FreeImage bitmap
-    int width = FreeImage_GetWidth(pBitmap);
-    int height = FreeImage_GetHeight(pBitmap);
-    unsigned int pitch = FreeImage_GetPitch(pBitmap);
-    BYTE *pFreeImageBits = FreeImage_GetBits(pBitmap);
+    int width = FreeImage_GetWidth(bitmap);
+    int height = FreeImage_GetHeight(bitmap);
+    unsigned int pitch = FreeImage_GetPitch(bitmap);
+    BYTE *p_bits = FreeImage_GetBits(bitmap);
 
     // Copy data from FreeImage bitmap to npp ImageCPU
-    T nppImage(width, height);
-    Npp8u *pNppData = nppImage.data(); // Pointer to NPP image data
+    T npp_image(width, height);
+    Npp8u *npp_data = npp_image.data();
     for (int y = 0; y < height; ++y)
     {
-        // Source row pointer in FreeImage bitmap
-        const BYTE *pSourceRow = pFreeImageBits + y * pitch;
-        Npp8u *pDestRow = pNppData + y * pitch;
-        std::memcpy(pDestRow, pSourceRow, pitch);
+        // source row pointer in FreeImage bitmap
+        const BYTE *row_src = p_bits + y * pitch;
+        // destination row pointer in NPP image
+        Npp8u *dst_row = npp_data + y * pitch;
+        std::memcpy(dst_row, row_src, pitch);
     }
-    // swap the user given image with our result image, effecively
-    // moving our newly loaded image data into the user provided shell
-    outImage.swap(nppImage);
+    // swap the user given image with our result image
+    out_image.swap(npp_image);
 }
 
 
@@ -58,43 +56,43 @@ FIBITMAP* convertNppImageToFreeImage(const T &nppImage)
     const uint channels = pitch / width;
 
     // Create a FreeImage bitmap (RGB)
-    FIBITMAP *pBitmap = FreeImage_Allocate(width, height, channels*8);
-    if (!pBitmap)
+    FIBITMAP *bitmap = FreeImage_Allocate(width, height, channels*8);
+    if (!bitmap)
     {
         std::cerr << "Failed to allocate FIBITMAP!" << std::endl;
         return nullptr;
     }
 
-    BYTE *pFreeImageBits = FreeImage_GetBits(pBitmap);
-    // Copy data from npp::ImageCPU_8u_C4 to FreeImage bitmap
-    const Npp8u *pNppData = nppImage.data(); // Pointer to NPP image data
+    BYTE *p_bits = FreeImage_GetBits(bitmap);
+    // copy data from npp::ImageCPU_8u_C4 to FreeImage bitmap
+    const Npp8u *npp_data = nppImage.data();
     for (int y = 0; y < height; ++y)
     {
-        // Source row pointer in NPP image
-        const Npp8u *pSourceRow = pNppData + y * pitch;
-        // Destination row pointer in FreeImage bitmap
-        BYTE *pDestRow = pFreeImageBits + y * pitch;
-        memcpy(pDestRow, pSourceRow, pitch);
+        // source row pointer in NPP image
+        const Npp8u *row_src = npp_data + y * pitch;
+        // destination row pointer in FreeImage bitmap
+        BYTE *dst_row = p_bits + y * pitch;
+        memcpy(dst_row, row_src, pitch);
     }
 
-    return pBitmap;
+    return bitmap;
 }
 
 
 /// @brief Sets opacity for all pixels in an RGBA FreeImage bitmap.
-/// @param pBitmap Pointer to the FreeImage bitmap.
+/// @param bitmap Pointer to the FreeImage bitmap.
 /// @param alpha Alpha value for all pixels (from 0.0 to 1.0).
-void setAlpha(FIBITMAP *pBitmap, const double alpha)
+void setAlpha(FIBITMAP *bitmap, const double alpha)
 {
     BYTE opacity = std::min(alpha, (double)1.0) * 255;
-    unsigned int width = FreeImage_GetWidth(pBitmap);
-    unsigned int height = FreeImage_GetHeight(pBitmap);
+    unsigned int width = FreeImage_GetWidth(bitmap);
+    unsigned int height = FreeImage_GetHeight(bitmap);
 
     // Update the alpha channel with the custom opacity value
     // TODO: replace by call to FreeImage_SetTransparencyTable?
     for (unsigned int y = 0; y < height; y++)
     {
-        BYTE *scanLine = FreeImage_GetScanLine(pBitmap, y);
+        BYTE *scanLine = FreeImage_GetScanLine(bitmap, y);
         for (unsigned int x = 0; x < width; x++)
         {
             scanLine[FI_RGBA_ALPHA] = opacity;
@@ -104,26 +102,25 @@ void setAlpha(FIBITMAP *pBitmap, const double alpha)
     }
 }
 
-
 /// @brief Saves NPP image to a file in PNG format.
 /// @tparam T type of the NPP image stored in host memory (e.g., npp::ImageCPU_8u_C3).
 /// @param file The name of the file to save.
 /// @param folder The destination folder path.
-/// @param nppImageHost The NPP image object in host memory to be saved.
+/// @param npp_image_host The NPP image object in host memory to be saved.
 template <typename T>
-void saveImage(const std::string file, const std::string folder, T &nppImageHost)
+void saveImage(const std::string file, const std::string folder, T &npp_image_host)
 {
     // convert the NPP image to FreeImage
-    FIBITMAP *pBitmap = convertNppImageToFreeImage<T>(nppImageHost);
-    std::string filePathOut = folder;
+    FIBITMAP *bitmap = convertNppImageToFreeImage<T>(npp_image_host);
+    std::string file_path_out = folder;
     const size_t pos = file.find_last_of("/\\");
-    const std::string fileName = (pos != std::string::npos) ? file.substr(pos + 1) : file;
-    filePathOut += fileName;
+    const std::string file_name = (pos != std::string::npos) ? file.substr(pos + 1) : file;
+    file_path_out += file_name;
 
-    if (!FreeImage_Save(FIF_PNG, pBitmap, filePathOut.c_str(), PNG_DEFAULT))
+    if (!FreeImage_Save(FIF_PNG, bitmap, file_path_out.c_str(), PNG_DEFAULT))
         std::cerr << "Failed to save image!" << std::endl;
     
-    FreeImage_Unload(pBitmap);
+    FreeImage_Unload(bitmap);
 }
 
 
